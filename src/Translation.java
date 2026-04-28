918907352f9fcfa90f5f9fb54e5adf66a24423c9
shadowleafy <81775868+shadowleafy@users.noreply.github.com>
2026-04-28 10:19:01 -0700

    miku

---
+++ src/Translation.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+class Translation{
+
+    public static final Map<String, String> ENGLISH = Map.of(
+        
+    );
+
+    public static final Map<String, String> CHINESE = Map.of(
+
+    );
+
+    public static final Map<String, String> JAPANESE = Map.of(
+
+    );
+
+}
